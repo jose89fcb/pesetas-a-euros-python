258eb0c6b273d53bb79add8fe81469b88b2fa912
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-11-24 03:41:35 +0100

    Update pesetas a euros.py

--- pesetas a euros.py
+++ pesetas a euros.py
@@ -3,7 +3,7 @@ print("Debes de ingresar el valor sin el punto\n\nEjemplo de error => 13.000\n\n
 
 try:
 
- pesetas=int(input("\n\nDame la cantidad: "))
+ pesetas=int(input("\n\nCantidad en pesetas : "))
 except ValueError:
     print("Error!")
 
